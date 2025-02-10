45a80a0f25a3283f69ad3156d4487d090d8199e4
Kahan-CS <kdesai6826@conestogac.on.ca>
2025-02-10 05:20:14 -0500

    resume link update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -47,7 +47,7 @@ const NavBar = () => {
             />
             <NavBarLink
               name="Resume"
-              link={"https://drive.google.com/file/d/1tOEBpCOGOJFOuNNH4YHh2UnLahJYxYNX/view?usp=sharing"}
+              link={"https://drive.google.com/file/d/17o23vqe8EtQx956xSeezk_TNCbYWVZJj/view?usp=sharing"}
               button="yes"
               target="_blank"
             />
